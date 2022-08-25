63d0ca8b2492d685735162b78e2a9ebac982aa70
Kiana <kiana.k7597@gmail.com>
2022-08-25 15:44:08 +0100

    exercise 03_12

--- Practice/03_12/script.js
+++ Practice/03_12/script.js
@@ -6,3 +6,30 @@
  * - Create several objects using the class.
  * - Test the objecs by calling their properties and using their methods in the console.
  */
+
+ class Bedsheet {
+    constructor(
+       name,
+       colour, 
+       pattern, 
+       size,
+       brand,
+       usage
+    )
+   { this.name = name;
+    this.colour = colour;
+    this.pattern = pattern;
+    this.size = size;
+    this.brand = brand;
+    this.usage = usage;
+   }
+   currentlyUsed(usage) {
+    this.usage = usage;
+   }
+}
+
+const floralBedsheet = new Bedsheet("bedsheet 1", "white", "floral", "double", "Urban Outfitters", "Not in Use")
+const jungleBedsheet = new Bedsheet("bedsheet 2", "green and yellow", "plants and jungle","double", "IKEA", "In Use")
+
+console.log(floralBedsheet)
+console.log(jungleBedsheet)
